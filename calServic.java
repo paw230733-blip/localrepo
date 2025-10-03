6a5d39ea8cd6412f68cc19d823462ca79a3b12d2
pawan Rajput <paw230733@gmail.com>
2025-10-02 21:52:10 -0700

    change somethidn

---
+++ calServic.java
@@ -0,0 +1,25 @@
+package localrepo;
+
+public class calServic{
+    private String name;
+    private String email;
+    private String depertment;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDepertment() {
+        return depertment;
+    }
+    public void setDepertment(String depertment) {
+        this.depertment = depertment;
+    }
+}
